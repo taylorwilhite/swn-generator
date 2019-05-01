f73fac7ef4776f4181f26774946cd40b1ce510cf
Taylor Wilhite <taylorwilhite@gmail.com>
2019-05-01 16:58:54 -0700

    add icons for source and donations

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,6 +4,8 @@ import { Generators } from "../../constants/Generators";
 import { GeneratorSelect } from "../GeneratorSelect";
 import { generate } from "../../functions/generate";
 import Button from '@material-ui/core/Button';
+import {ReactComponent as GithubLogo} from '../../images/github-brands.svg';
+import kofi from '../../images/Ko-fi_Icon_Blue.png';
 
 export const App = () => {
   const [generated, setGenerated] = useState("");
@@ -16,6 +18,10 @@ export const App = () => {
       <Button variant="contained" color="primary" onClick={() => setGenerated(generate(generatorType.type))}>
         Generate { generatorType.type }
       </Button>
+      <div className="logos">
+        <a href="https://github.com/taylorwilhite/swn-generator" target="_blank"><GithubLogo className="grayscale icon" /></a>
+        <a href="https://ko-fi.com/tmoneybagels" target="_blank"><img className="grayscale icon" src={kofi} alt="Source Code"/></a>
+      </div>
     </div>
   );
 }
